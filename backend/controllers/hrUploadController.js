54f8e74a9d13c677a3badb4d869690b366572b69
unknown <Jude David Acula@LAPTOP-PQOHFF1D.NMCP2021.com>
2023-10-26 09:38:53 +0800

    update backend

--- backend/controllers/hrUploadController.js
+++ backend/controllers/hrUploadController.js
@@ -5,7 +5,7 @@ module.exports = {
     async upload(req, res) {
         hr_upload(req, res, async (err) => {
             let hr_upload_class = new hrUploadClass(req.body, req.file);
-            console.log(req);
+
             let upload = await hr_upload_class.upload();
 
             res.send(upload);
@@ -18,5 +18,31 @@ module.exports = {
         let uploads = await hr_upload_class.getUpload();
 
         res.send(uploads);
+    },
+
+    async getUploadItems(req, res) {
+        let hr_upload_class = new hrUploadClass(req.body);
+
+        let uploads = await hr_upload_class.getUploadItems();
+
+        res.send(uploads);
+    },
+
+    async updateItem(req, res) {
+        hr_upload(req, res, async (err) => {
+            let hr_upload_class = new hrUploadClass(req.body, req.file);
+
+            let upload = await hr_upload_class.updateItem();
+
+            res.send(upload);
+        });
+    },
+
+    async getByMenu(req, res) {
+        let hr_upload_class = new hrUploadClass(req.body);
+
+        let upload = await hr_upload_class.getByMenu();
+
+        res.send(upload);
     }
 }
