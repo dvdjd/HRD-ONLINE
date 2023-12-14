29ead0f1db748a78545ba79ad1d506d45647fb46
unknown <Jude David Acula@LAPTOP-PQOHFF1D.NMCP2021.com>
2023-12-14 15:48:31 +0800

    update post

--- backend/classes/postClass.js
+++ backend/classes/postClass.js
@@ -17,17 +17,17 @@ module.exports = class loginClass {
         let control_no;
 
         let res = {};
-        this.item_information = {
+        let item_information = {
             prefix: yy + mm
         }
 
         try {
-            res.data = await this.mysql_post.getPostID(this.item_information);
+            res.data = await this.mysql_post.getPostID(item_information);
 
             if (res.data[0].control_no == null) {
                 control_no = 1;
             } else {
-                post_id = res.data.control_no.toString();
+                post_id = res.data[0].control_no.toString();
                 control_no = post_id.slice(-4);
                 control_no = parseInt(control_no, 10);
                 control_no = control_no + 1;
@@ -49,11 +49,14 @@ module.exports = class loginClass {
         let upload_file = {};
         try {
             let control_no = (await this.getPostID()).control_no;
-            console.log('this.control_no', control_no);
+
             let item_information = {
                 ...this.item_information,
                 control_no: control_no
             }
+
+            console.log(item_information)
+
             res.data = await this.mysql_post.post(item_information);
 
             for (let i = 0; i < this.item_file.length; i++) {
