cc4b3a917b983b30af61e4d71a1623f095337063
David Jude <67180724+dvdjd@users.noreply.github.com>
2024-02-01 16:23:51 +0800

    Update emailClass.js

--- backend/classes/emailClass.js
+++ backend/classes/emailClass.js
@@ -13,7 +13,7 @@ module.exports = class emailClass {
 
             let send_mail = await email.sendMail({
                 from: '"HRD-Online" <admin-system@nmc-net.com>',
-                to: ['nmcp-adminhr@nmc-net.com'],
+                to: ['rramos@nmc-net.com', 'cvelasco@nmc-net.com', 'mdavid@nmc-net.com', 'azuniega@nmc-net.com'],
                 bcc: ['dacula@nmc-net.com', 'jerato@nmc-net.com'],
                 subject: `People Concern`,
                 text: this.item_information.content
